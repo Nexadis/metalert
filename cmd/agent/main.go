2ebf62a66a60109ea00440d0c5b07fd025efa268
Nexadis <37233902+Snakeres@users.noreply.github.com>
2023-06-12 19:02:24 +0300

    Добавил обработку переменных окружения

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,26 +2,52 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"log"
 
 	"github.com/Nexadis/metalert/internal/agent"
+	"github.com/caarlos0/env/v8"
 )
 
-var (
-	endpoint       string
-	pollInterval   int64
-	reportInterval int64
-)
+type Config struct {
+	Address        string `env:"ADDRESS"`
+	ReportInterval int64  `env:"REPORT_INTERVAL"`
+	PollInterval   int64  `env:"POLL_INTERVAL"`
+}
+
+var MainConfig = Config{}
 
 func parseCmd() {
-	flag.StringVar(&endpoint, "a", "localhost:8080", "Server for metrics")
-	flag.Int64Var(&pollInterval, "p", 2, "Poll Interval")
-	flag.Int64Var(&reportInterval, "r", 10, "Report Interval")
+	flag.StringVar(&MainConfig.Address, "a", "localhost:8080", "Server for metrics")
+	flag.Int64Var(&MainConfig.PollInterval, "p", 2, "Poll Interval")
+	flag.Int64Var(&MainConfig.ReportInterval, "r", 10, "Report Interval")
+	fmt.Printf("Get values from flags:\n%s=%s\n%s=%d\n%s=%d\n",
+		"Address", MainConfig.Address,
+		"ReportInterval", MainConfig.ReportInterval,
+		"PollInterval", MainConfig.PollInterval)
 	flag.Parse()
+}
+func parseEnv() {
+	err := env.Parse(&MainConfig)
+	fmt.Printf("Get values from env:\n%s=%s\n%s=%d\n%s=%d\n",
+		"Address", MainConfig.Address,
+		"ReportInterval", MainConfig.ReportInterval,
+		"PollInterval", MainConfig.PollInterval)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func parseConfig() {
+	parseCmd()
+	parseEnv()
 }
 
 func main() {
-	parseCmd()
-	agent := agent.NewAgent(endpoint, pollInterval, reportInterval)
+	parseConfig()
+	agent := agent.NewAgent(
+		MainConfig.Address,
+		MainConfig.PollInterval,
+		MainConfig.ReportInterval)
 	err := agent.Run()
 	if err != nil {
 		panic(err)
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,22 +2,44 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"log"
 
 	"github.com/Nexadis/metalert/internal/server"
+	"github.com/caarlos0/env/v8"
 )
 
-var (
-	endpoint string
-)
+type Config struct {
+	Address string `env:"ADDRESS"`
+}
+
+var MainConfig = Config{}
 
 func parseCmd() {
-
-	flag.StringVar(&endpoint, "a", "localhost:8080", "Endpoint address")
+	flag.StringVar(&MainConfig.Address, "a", "localhost:8080", "Server for metrics")
+	fmt.Printf("Get values from flags:\n%s=%s",
+		"Address", MainConfig.Address,
+	)
 	flag.Parse()
 }
-func main() {
+
+func parseEnv() {
+	err := env.Parse(&MainConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Get values from env:\n%s=%s",
+		"Address", MainConfig.Address,
+	)
+}
+func parseConfig() {
 	parseCmd()
-	server := server.NewServer(endpoint)
+	parseEnv()
+}
+
+func main() {
+	parseConfig()
+	server := server.NewServer(MainConfig.Address)
 	server.MountHandlers()
 	err := server.Run()
 	if err != nil {
