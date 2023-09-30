7b3bd63492274174e5826627267250a503ffbf23
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-09-30 21:19:14 +0300

    стандартные и staticheck.io чекеры

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.20
 
 require (
 	github.com/caarlos0/env/v8 v8.0.0
-	github.com/fatih/color v1.15.0
 	github.com/go-chi/chi/v5 v5.0.8
 	github.com/go-resty/resty/v2 v2.7.0
 	github.com/jackc/pgerrcode v0.0.0-20220416144525-469b46aa5efa
@@ -12,6 +11,8 @@ require (
 	github.com/shirou/gopsutil/v3 v3.23.6
 	github.com/stretchr/testify v1.8.4
 	go.uber.org/zap v1.24.0
+	golang.org/x/tools v0.13.0
+	honnef.co/go/tools v0.4.6
 )
 
 require (
@@ -21,24 +22,20 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/rogpeppe/go-internal v1.11.0 // indirect
+	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/tklauser/go-sysconf v0.3.11 // indirect
 	github.com/tklauser/numcpus v0.6.0 // indirect
-	github.com/yuin/goldmark v1.5.6 // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/exp/typeparams v0.0.0-20221208152030-732eee02a75a // indirect
 	golang.org/x/mod v0.12.0 // indirect
 	golang.org/x/net v0.15.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
-	golang.org/x/tools v0.13.0 // indirect
-	golang.org/x/tour v0.1.0 // indirect
-	golang.org/x/website/tour v0.0.0-20210616181959-e0d934b43647 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/analyzer/analyzer.go
+++ internal/analyzer/analyzer.go
@@ -1,13 +1,94 @@
 package analyzer
 
-import "golang.org/x/tools/go/analysis"
+import (
+	"strings"
+
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/asmdecl"
+	"golang.org/x/tools/go/analysis/passes/assign"
+	"golang.org/x/tools/go/analysis/passes/atomic"
+	"golang.org/x/tools/go/analysis/passes/bools"
+	"golang.org/x/tools/go/analysis/passes/buildtag"
+	"golang.org/x/tools/go/analysis/passes/cgocall"
+	"golang.org/x/tools/go/analysis/passes/composite"
+	"golang.org/x/tools/go/analysis/passes/copylock"
+	"golang.org/x/tools/go/analysis/passes/errorsas"
+	"golang.org/x/tools/go/analysis/passes/httpresponse"
+	"golang.org/x/tools/go/analysis/passes/loopclosure"
+	"golang.org/x/tools/go/analysis/passes/lostcancel"
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
+	"golang.org/x/tools/go/analysis/passes/printf"
+	"golang.org/x/tools/go/analysis/passes/shift"
+	"golang.org/x/tools/go/analysis/passes/stdmethods"
+	"golang.org/x/tools/go/analysis/passes/structtag"
+	"golang.org/x/tools/go/analysis/passes/tests"
+	"golang.org/x/tools/go/analysis/passes/unmarshal"
+	"golang.org/x/tools/go/analysis/passes/unreachable"
+	"golang.org/x/tools/go/analysis/passes/unsafeptr"
+	"honnef.co/go/tools/staticcheck"
+)
 
 var ExitCheckAnalyzer = &analysis.Analyzer{
 	Name: "exitcheck",
-	Doc:  "Проверяет наличие exit фукнций в main функциях main-пакетов",
+	Doc:  "Проверяет наличие exit функций в main функциях main-пакетов",
 	Run:  run,
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
 	return nil, nil
+}
+
+func StandardPasses() []*analysis.Analyzer {
+	return []*analysis.Analyzer{
+		asmdecl.Analyzer,
+		assign.Analyzer,
+		atomic.Analyzer,
+		bools.Analyzer,
+		buildtag.Analyzer,
+		cgocall.Analyzer,
+		composite.Analyzer,
+		copylock.Analyzer,
+		errorsas.Analyzer,
+		httpresponse.Analyzer,
+		loopclosure.Analyzer,
+		lostcancel.Analyzer,
+		nilfunc.Analyzer,
+		printf.Analyzer,
+		shift.Analyzer,
+		stdmethods.Analyzer,
+		structtag.Analyzer,
+		tests.Analyzer,
+		unmarshal.Analyzer,
+		unreachable.Analyzer,
+		unsafeptr.Analyzer,
+	}
+}
+
+func StaticChecks() []*analysis.Analyzer {
+	needChecks := []string{
+		"SA",
+		"S100",
+		"ST",
+		"QF1002",
+		"QF1003",
+		"QF1006",
+		"QF1010",
+		"QF1012",
+	}
+	staticchecks := make([]*analysis.Analyzer, 0, 20)
+	for _, a := range staticcheck.Analyzers {
+		for _, need := range needChecks {
+			if strings.HasPrefix(a.Analyzer.Name, need) {
+				staticchecks = append(staticchecks, a.Analyzer)
+			}
+		}
+	}
+	return staticchecks
+}
+
+func AllAnalyzers() []*analysis.Analyzer {
+	a := make([]*analysis.Analyzer, 0, 100)
+	a = append(a, StaticChecks()...)
+	a = append(a, StandardPasses()...)
+	return a
 }
