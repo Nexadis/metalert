0edf1fc0582becdfee4300e94716d34bdbe0f40b
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-10-02 18:19:00 +0300

    Вызов exit не из main функции в main пакете

--- internal/analyzer/testdata/main/main.go
+++ internal/analyzer/testdata/main/main.go
@@ -4,7 +4,11 @@ import (
 	"os"
 )
 
-func errCheckFunc() {
+func some() {
+	os.Exit(1)
+}
+
+func main() {
 	// формулируем ожидания: анализатор должен находить ошибку,
 	// описанную в комментарии want
 	os.Exit(0) // want "don't use exit in main"
