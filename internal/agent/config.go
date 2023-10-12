b6ddd491679d09a4bc479dd0404fce937fae967a
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-10-12 16:27:23 +0300

    Завершение работы агента по сигналу

--- internal/agent/config.go
+++ internal/agent/config.go
@@ -15,6 +15,7 @@ type Config struct {
 	PollInterval   int64  `env:"POLL_INTERVAL"`
 	Key            string `env:"KEY"`        // ключ для подписи отправляемых метрик
 	RateLimit      int64  `env:"RATE_LIMIT"` // количество воркеров для отправки метрик
+	Verbose        bool   `env:"VERBOSE"`    // Включить логгирование
 }
 
 func NewConfig() *Config {
@@ -28,6 +29,7 @@ func (c *Config) parseCmd() {
 	flag.Int64Var(&c.ReportInterval, "r", 10, "Report Interval")
 	flag.StringVar(&c.Key, "k", "", "Key to sign body")
 	flag.Int64Var(&c.RateLimit, "l", 1, "Workers for report")
+	flag.BoolVar(&c.Verbose, "v", false, "Verbose logging")
 	flag.Parse()
 }
 
@@ -42,6 +44,9 @@ func (c *Config) parseEnv() {
 func (c *Config) ParseConfig() {
 	c.parseCmd()
 	c.parseEnv()
+	if c.Verbose {
+		logger.Enable()
+	}
 	logger.Info("Parsed Config:",
 		"Address", c.Address,
 		"ReportInterval", c.ReportInterval,
