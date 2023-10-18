b8e41c8c61709360e6c4e63e851c5ba853f53c87
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-10-18 20:10:57 +0300

    Middleware для Расшифровывания трафика

---
+++ internal/server/middlewares/decrypt.go
@@ -0,0 +1,38 @@
+package middlewares
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+
+	"github.com/Nexadis/metalert/internal/utils/asymcrypt"
+	"github.com/Nexadis/metalert/internal/utils/logger"
+)
+
+func WithDecrypt(h http.Handler, privKey []byte) http.Handler {
+	decrypt := func(w http.ResponseWriter, r *http.Request) {
+		if privKey == nil {
+			logger.Info("No key, no decrypt")
+			h.ServeHTTP(w, r)
+			return
+		}
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer r.Body.Close()
+		logger.Info("Begin Decrypt")
+		decrypted, err := asymcrypt.Decrypt(body, privKey)
+		if err != nil {
+			logger.Error(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		r.Body = io.NopCloser(bytes.NewReader(decrypted))
+		logger.Info("Decrypted data:", string(decrypted))
+
+		h.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(decrypt)
+}
