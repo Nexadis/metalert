676d2a0d095cff076dff293dc08640d744673330
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-11-04 11:39:26 +0300

    Переписал выбор клиента, чтобы пакет предоставлял различными методами различных клиентов на выбор

---
+++ internal/agent/client/http_client.go
@@ -0,0 +1,183 @@
+// client реализует клиента для отправки метрик по HTTP
+package client
+
+import (
+	"bytes"
+	"compress/gzip"
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/go-resty/resty/v2"
+
+	"github.com/Nexadis/metalert/internal/models"
+	"github.com/Nexadis/metalert/internal/utils/asymcrypt"
+	"github.com/Nexadis/metalert/internal/utils/verifier"
+)
+
+type httpType int
+
+const (
+	JSONType httpType = iota
+	RESTType
+)
+
+// Endpoint'ы для отправки метрик.
+const (
+	UpdateURL     = "/update/{valType}/{name}/{value}"
+	JSONUpdateURL = "/update/"
+	// Для отправки сразу пачки метрик
+	JSONUpdatesURL = "/updates/"
+)
+
+// MetricPoster интерфейс для отправки метрик как через URL, так и JSON-объектами.
+type MetricPoster interface {
+	Post(ctx context.Context, server string, m models.Metric) error
+}
+
+// httpClient отправляет метрики и подписывает их ключом key.
+type httpClient struct {
+	client    *resty.Client
+	signkey   string
+	pubkey    []byte
+	transport httpType
+}
+
+func newClient(options ...FOption) *httpClient {
+	client := &httpClient{
+		client: resty.New().
+			SetRetryCount(3).
+			SetRetryWaitTime(1 * time.Second).
+			SetRetryMaxWaitTime(5 * time.Second),
+	}
+	for _, o := range options {
+		o(client)
+	}
+	return client
+}
+
+// NewREST - создаёт httpClient для отправки метрик через REST, принимает в качестве аргументов функции, например:
+//
+// func SetKey(key string) func(*httpClient)
+func NewREST(options ...FOption) *httpClient {
+	c := newClient(options...)
+	c.transport = RESTType
+	return c
+}
+
+// NewJSON - создаёт httpClient для отправки метрик через REST, принимает в качестве аргументов функции, например:
+//
+// func SetKey(key string) func(*httpClient)
+func NewJSON(options ...FOption) *httpClient {
+	c := newClient(options...)
+	c.transport = JSONType
+	return c
+}
+
+func (c *httpClient) Post(ctx context.Context, server string, m models.Metric) error {
+	switch c.transport {
+	case RESTType:
+		return c.postREST(ctx, server, m)
+	case JSONType:
+		return c.postJSON(ctx, server, m)
+	}
+	return fmt.Errorf("unknown transport type")
+}
+
+// Post отправляет метрику через REST-запрос
+//
+// path - адрес сервера, например "localhost:8080"
+func (c *httpClient) postREST(ctx context.Context, server string, m models.Metric) error {
+	val, err := m.GetValue()
+	if err != nil {
+		return err
+	}
+	realIP, err := getRealIP()
+	if err != nil {
+		return err
+	}
+
+	query := fmt.Sprintf("http://%s%s", server, UpdateURL)
+	_, err = c.client.R().
+		SetContext(ctx).
+		SetHeader("Content-type", "text/plain").
+		SetHeader("Accept-Encoding", "gzip").
+		SetHeader("X-Real-IP", realIP.String()).
+		SetPathParams(map[string]string{
+			"valType": m.MType,
+			"name":    m.ID,
+			"value":   val,
+		}).Post(query)
+
+	return err
+}
+
+// postJSON отправляет метрику в виде JSON-строки, дополнительно сжимая её с помощью gzip и подписывая с помощью httpClient.key.
+func (c *httpClient) postJSON(ctx context.Context, server string, m models.Metric) error {
+	buf, err := json.Marshal(m)
+	if err != nil {
+		return err
+	}
+	encrypted := buf
+	if c.pubkey != nil {
+		encrypted, err = asymcrypt.Encrypt(buf, c.pubkey)
+		if err != nil {
+			return err
+		}
+	}
+	body := &bytes.Buffer{}
+	g := gzip.NewWriter(body)
+	_, err = g.Write(encrypted)
+	if err != nil {
+		return err
+	}
+	err = g.Close()
+	if err != nil {
+		return err
+	}
+	realIP, err := getRealIP()
+	if err != nil {
+		return err
+	}
+
+	Headers := map[string]string{
+		"Content-type":     "application/json",
+		"Accept-Encoding":  "gzip",
+		"Content-Encoding": "gzip",
+		"X-Real-IP":        realIP.String(),
+	}
+	if c.signkey != "" {
+		signature, err := verifier.Sign(buf, []byte(c.signkey))
+		if err != nil {
+			return err
+		}
+		Headers[verifier.HashHeader] = base64.StdEncoding.EncodeToString(signature)
+	}
+	query := fmt.Sprintf("http://%s%s", server, JSONUpdateURL)
+
+	_, err = c.client.R().
+		SetContext(ctx).
+		SetHeaders(Headers).
+		SetBody(body).
+		Post(query)
+	return err
+}
+
+func getRealIP() (net.Addr, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return nil, err
+	}
+	var realIP net.Addr
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				realIP = a
+			}
+		}
+	}
+	return realIP, nil
+}
---
+++ internal/agent/client/http_client_test.go
@@ -0,0 +1,239 @@
+package client
+
+import (
+	"compress/gzip"
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/Nexadis/metalert/internal/models"
+	"github.com/Nexadis/metalert/internal/utils/asymcrypt"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHTTP(t *testing.T) {
+	c := NewJSON(SetSignKey("test-key"))
+	assert.NotNil(t, c)
+	c = NewREST(SetSignKey("test-key"))
+	assert.NotNil(t, c)
+}
+
+type reqLogger struct {
+	method string
+	body   string
+	url    string
+}
+
+func (l *reqLogger) showHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return
+	}
+	defer r.Body.Close()
+	l.body = string(body)
+	l.url = r.URL.Path
+	l.method = r.Method
+}
+
+type metric struct {
+	name  string
+	mtype string
+	val   string
+}
+
+type want struct {
+	method string
+	body   string
+	url    string
+	err    error
+}
+
+type testReq struct {
+	name string
+	m    metric
+	want want
+}
+
+var postTests = []testReq{
+	{
+		"Post URL gauge",
+		metric{
+			"name",
+			"gauge",
+			"123.123",
+		},
+		want{
+			http.MethodPost,
+			"",
+			"/update/gauge/name/123.123",
+			nil,
+		},
+	},
+	{
+		"Post URL counter",
+		metric{
+			"newname",
+			"counter",
+			"1230",
+		},
+		want{
+			http.MethodPost,
+			"",
+			"/update/counter/newname/1230",
+			nil,
+		},
+	},
+	{
+		"Post URL invalid",
+		metric{
+			"name",
+			"invalid",
+			"some-value",
+		},
+		want{
+			http.MethodPost,
+			"",
+			"/update/invalid/name/some-value",
+			models.ErrorType,
+		},
+	},
+}
+
+func TestPostREST(t *testing.T) {
+	r := reqLogger{}
+	s := httptest.NewServer(http.HandlerFunc(r.showHandler))
+	defer s.Close()
+	c := NewREST(SetSignKey("test-key"))
+	ctx := context.Background()
+	path := s.URL[len("http://"):]
+	for _, test := range postTests {
+		t.Run(test.name, func(t *testing.T) {
+			m, err := models.NewMetric(test.m.name, test.m.mtype, test.m.val)
+			if test.want.err != nil {
+				assert.Error(t, err)
+				return
+			}
+			assert.NoError(t, err)
+			err = c.Post(ctx, path, m)
+			assert.NoError(t, err)
+			assert.Equal(t, test.want.url, r.url)
+			assert.Equal(t, test.want.body, r.body)
+			assert.Equal(t, test.want.method, r.method)
+		},
+		)
+	}
+}
+
+var postObjTests = []testReq{
+	{
+		"Just object",
+		metric{
+			"name",
+			"",
+			"",
+		},
+		want{
+			http.MethodPost,
+			"\"name\"",
+			"/update",
+			models.ErrorMetrics,
+		},
+	},
+	{
+		"Valid counter",
+		metric{
+			"somec",
+			models.CounterType,
+			"123",
+		},
+		want{
+			http.MethodPost,
+			"",
+			"/update/",
+			nil,
+		},
+	},
+	{
+		"Valid gauge",
+		metric{
+			"someg",
+			models.GaugeType,
+			"123",
+		},
+		want{
+			http.MethodPost,
+			"",
+			"/update/",
+			nil,
+		},
+	},
+}
+
+type testJSON struct {
+	name   string
+	metric models.Metric
+	want   want
+}
+
+func prepareJSONtests(tests []testReq) []testJSON {
+	jsons := make([]testJSON, 0, len(postObjTests))
+	for _, test := range tests {
+
+		m, err := models.NewMetric(test.m.name, test.m.mtype, test.m.val)
+		if err != nil {
+			continue
+		}
+		jsoned, err := json.Marshal(m)
+		test.want.body = string(jsoned)
+		if err != nil {
+			continue
+		}
+		jsons = append(jsons, testJSON{
+			test.name,
+			m,
+			test.want,
+		})
+	}
+	return jsons
+}
+
+func TestPostJSON(t *testing.T) {
+	r := reqLogger{}
+	s := httptest.NewServer(http.HandlerFunc(r.showHandler))
+	defer s.Close()
+	keyname := os.TempDir() + "/test-key"
+	err := asymcrypt.NewPem(keyname)
+	assert.NoError(t, err)
+	pub, err := asymcrypt.ReadPem(keyname + "_pub.pem")
+	assert.NoError(t, err)
+	priv, err := asymcrypt.ReadPem(keyname + "_priv.pem")
+	assert.NoError(t, err)
+	c := NewJSON(SetSignKey("test-key"), SetPubKey(pub))
+	ctx := context.Background()
+	path := s.URL[len("http://"):]
+	tests := prepareJSONtests(postObjTests)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert.NoError(t, err)
+			err = c.Post(ctx, path, test.metric)
+			assert.NoError(t, err)
+			assert.Equal(t, test.want.url, r.url)
+			body := strings.NewReader(r.body)
+			g, err := gzip.NewReader(body)
+			assert.NoError(t, err)
+			buf, err := io.ReadAll(g)
+			assert.NoError(t, err)
+			decrypted, err := asymcrypt.Decrypt(buf, priv)
+			assert.NoError(t, err)
+			assert.NoError(t, g.Close())
+			assert.JSONEq(t, test.want.body, string(decrypted))
+			assert.Equal(t, test.want.method, r.method)
+		},
+		)
+	}
+}
