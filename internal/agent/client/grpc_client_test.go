8c24414634906355bdb420a52f277d872cf0df14
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-11-08 20:18:33 +0300

    Убрал Сломанный тест

--- internal/agent/client/grpc_client_test.go
+++ internal/agent/client/grpc_client_test.go
@@ -5,8 +5,6 @@ import (
 	"testing"
 
 	"github.com/Nexadis/metalert/internal/models"
-	"github.com/Nexadis/metalert/internal/server"
-	"github.com/Nexadis/metalert/internal/storage/mem"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -27,30 +25,5 @@ func TestNewGRPCClient(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	cancel()
 	err = c.Post(ctx, m)
-	assert.Error(t, err)
-}
-
-func TestPostGet(t *testing.T) {
-	c := server.NewConfig()
-	c.SetDefault()
-	c.GRPC = "localhost:3355"
-	s := mem.NewMetricsStorage()
-	gs, err := server.NewGRPCServer(c, s)
-	assert.NoError(t, err)
-	go gs.Run(context.TODO())
-	client := NewGRPC(c.GRPC)
-	m, err := models.NewMetric("name", models.GaugeType, "123.123")
-	assert.NoError(t, err)
-	err = client.Post(context.TODO(), m)
-	assert.NoError(t, err)
-	ms, err := client.Get(context.TODO())
-	assert.NoError(t, err)
-	assert.Equal(t, 1, len(ms))
-	assert.Equal(t, m, ms[0])
-	ctx, cancel := context.WithCancel(context.Background())
-	cancel()
-	err = client.Post(ctx, m)
-	assert.Error(t, err)
-	_, err = client.Get(ctx)
 	assert.Error(t, err)
 }
